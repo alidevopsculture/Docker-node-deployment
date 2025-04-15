d7282842e424d9355f6696e6f6828bd247f66bb3
MacBook <macbook@Alis-MacBook-Pro.local>
2025-04-15 14:16:56 +0530

    text-added

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 Hello from Ali's Dockerized Node.js App!");
+  res.send("🚀 Hello from Ali's Dockerized Node.js App! this is shaban khan and i want you guys to appriciate that i deplpoyed my first app thanks!!");
 });
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`I can see you at--- ${PORT}`);
 });
